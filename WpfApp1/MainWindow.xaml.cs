1974a6e400343670790185284e41cb8572be7874
supernikd <supernikd@gmail.com>
2019-01-16 15:03:06 +0200

    h

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -20,9 +23,29 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Tick += new EventHandler(Timer_tick);
+            timer.Interval = new TimeSpan(0, 0, 5);
+            timer.Start();
+
+        }
+        private void Timer_tick(object sender, EventArgs e)
+        {
+            int rnd_x = rnd.Next(5, 70);
+            Ellipse el = new Ellipse()
+            {
+                Width = rnd_x,
+                Height= rnd_x
+            };
+            el.Fill = Brushes.Green;
+            Cnvs.Children.Add(el);
+            Canvas.SetTop(el, rnd.Next(20, 500));
+            Canvas.SetLeft(el,rnd.Next(20, 500));
         }
     }
 }
